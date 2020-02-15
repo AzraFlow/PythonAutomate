36ac463c596edc62e3c4ae4b3d7f990879357d69
azraflow <azraflow@gmail.com>
2020-02-15 12:07:11 -0500

    Added input verification exercise using PyInputPlus

---
+++ Chap8/idiot.py
@@ -0,0 +1,12 @@
+import pyinputplus as pyip
+
+while True:
+    prompt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(prompt)
+    if response == 'no':
+        break
+
+
+
+
+print('Thank You.  Have a nice day.')
