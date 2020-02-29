e67ce6f0e6854ce2b9ba474b583fdadb8034d60d
azraflow <azraflow@gmail.com>
2020-02-29 15:36:50 -0500

    Add coinFlip.py and coinTossDebug.py files.

---
+++ Chap11/coinTossDebug.py
@@ -0,0 +1,23 @@
+import logging
+logging.disable(logging.CRITICAL)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s- %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s- %(levelname)s- %(message)s')
+logging.debug('Start of program')
+import random
+coin =['heads', 'tails']
+guess = ''
+while guess not in coin:
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+    logging.debug('Guess entered is (%s)' %guess)
+toss = coin[random.randint(0, 1)]  # 0 is tails, 1 is heads
+logging.debug('Toss is (%s)' %toss)
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
