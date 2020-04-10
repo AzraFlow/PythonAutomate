1388f1a5f86c97e4880b8de977dad7fb9172058a
azraflow <azraflow@gmail.com>
2020-04-10 10:57:00 -0400

    Added chapter 17 exercises and cleaned up other files using Flake8 linter.

--- Chap11/coinTossDebug.py
+++ Chap11/coinTossDebug.py
@@ -1,17 +1,20 @@
-import logging
-logging.disable(logging.CRITICAL)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s- %(levelname)s- %(message)s')
-logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s- %(levelname)s- %(message)s')
-logging.debug('Start of program')
 import random
-coin =['heads', 'tails']
+import logging
+logging.disable(logging.CRITICAL)  # uncomment to disable logging messages
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s- %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.CRITICAL,
+                    format='%(asctime)s- %(levelname)s- %(message)s')
+logging.debug('Start of program')
+
+coin = ['heads', 'tails']
 guess = ''
 while guess not in coin:
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
-    logging.debug('Guess entered is (%s)' %guess)
+    logging.debug('Guess entered is (%s)' % guess)
 toss = coin[random.randint(0, 1)]  # 0 is tails, 1 is heads
-logging.debug('Toss is (%s)' %toss)
+logging.debug('Toss is (%s)' % toss)
 if toss == guess:
     print('You got it!')
 else:
