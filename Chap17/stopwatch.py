aee9b3e1226a508e452cc46fbad638749e0ca0bd
azraflow <azraflow@gmail.com>
2020-04-11 20:55:15 -0400

    Small corrections for Chap17 files.

--- Chap17/stopwatch.py
+++ Chap17/stopwatch.py
@@ -5,7 +5,7 @@ import time
 
 # Display the program's instructions.
 print('Press ENTER to begin.  Afterward, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
-input() #press Enter to begin
+input()  # press Enter to begin
 print('Started.')
 startTime = time.time()  # get the first lap's start time
 lastTime = startTime
@@ -17,9 +17,9 @@ try:
         input()
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
-        print('Lap #%s: %s (%s)' %(lapNum, totalTime, lapTime), end='')
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
         lapNum += 1
-        lastTime = time.time() # reset the last lap times
+        lastTime = time.time()  # reset the last lap times
 except KeyboardInterrupt:
     # Handle the Ctrl-C exception to keep its error message from displaying.
     print('\nDone.')
